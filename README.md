4633a25f0ec4c83cb8db7ab3e7f4477266cc95b1
daniele campari (dak79) <danielecampari@gmail.com>
2021-07-07 16:44:43 +0300

    app routes initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# covidTraker
+# COVID TRACKER
+#### Video Demo:  <URL HERE>
+#### Description:
+TODO
+
+
+NOTE
+Your README.md file should be minimally multiple paragraphs in length, and should explain what your project is, what each of the files you wrote for the project contains and does, and if you debated certain design choices, explaining why you made them. Ensure you allocate sufficient time and energy to writing a README.md that you are proud of and that documents your project thoroughly. Be proud of it!
---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+
+# Initializing app
+app = Flask(__name__)
+
+
+# Route to homepage
+@app.route('/')
+def index():
+    # TODO: display dinamic world data from api, manage buttons, mamage dynamic navbar
+    return render_template('index.html')
+
+
+# Route to register
+@app.route('/register')
+def register():
+    # TODO: Username unique and min 4 char, password should match with confirm e min char=4, mail should be a mail. Search form validator for flask.
+    # TODO: display clear message for validation, disply message if registration successful
+    # TODO: hash password before saving on database
+    # TODO: store form in database
+    return render_template('register.html')
+
+
+# Route to login
+@app.route('/login')
+def login():
+    # TODO: check if data are in database,
+    # TODO: dehash password for chacking without storing the value in a var,
+    # TODO: redirect to dashbord if login is successful and message the user,
+    # TODO: dipslay a message if error occure
+    # TODO: open session
+    return render_template('login.html')
+
+
+# Route to logout
+@app.route('/logout')
+def logout():
+    # TODO: logout from session
+    return redirect('/')
+
+
+# Route to dashboard
+@app.route('/dashboard')
+def dashboard():
+    # TODO: only if logged in
+    return render_template('dashboard.html')
+
+
+# Route to add country
+@app.route('/add_country')
+def add_country():
+
+    # TODO: show all country
+    # TODO: think if it is possible using directly by_country.html
+    return render_template('add_country.html')
+
+
+# Route to data by_country
+@app.route('/by_country')
+def by_country():
+    return render_template('by_country.html')
+
+
+# Route to vaccination data
+@app.route('/vaccination')
+def vaccination():
+    return render_template('vaccination.html')
+
+
+# Lunch app
+if __name__ == '__main__':
+    app.run(debug=True)
