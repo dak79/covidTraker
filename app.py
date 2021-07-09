35960b4e034029ac9daca6cfd930a4169bc640ea
daniele campari (dak79) <danielecampari@gmail.com>
2021-07-09 13:59:42 +0300

    register, login, logout

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 from forms import RegistrationForm, LoginForm
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+import os
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
 # Initializing app
 app = Flask(__name__)
+
+# Config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///covidtracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+# Inizialize db
+db = SQLAlchemy(app)
+
+# Inizialize Session
+Session(app)
+
+
+# Create database
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
 
 # Route to homepage
 @app.route('/')
 def index():
-    # TODO: display dinamic world data from api, manage buttons, mamage dynamic navbar
+    # TODO: display dinamic world data from api, manage buttons, manage dynamic navbar
     return render_template('index.html')
 
 
 # Route to register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    ''' User registration '''
 
     # Instance form
     form = RegistrationForm(request.form)
@@ -28,21 +61,26 @@ def register():
         email = form.email.data
         psw = form.password.data
 
-        # Debug
-        print(user, email, psw)
+        # Checking username already existing
+        result = Users.query.filter_by(username=user).first()
+        if result != None:
+            flash('Username already exist', 'danger')
+            return redirect('/register')
 
-        # grab all users from db, check if user exist
+        # Insert new user in db
+        new_user = Users(user, email, generate_password_hash(psw))
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You are successfully registered, please log in', 'success')
+        return redirect('/login')
 
-    # TODO: grab all users from db, check if user exist. User shoul be unique
-    # TODO: disply message if registration successful
-    # TODO: hash password before saving on database
-    # TODO: store form in database
     return render_template('register.html', form=form)
 
 
 # Route to login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    ''' User Log In '''
 
     # Instance Form
     form = LoginForm(request.form)
@@ -50,29 +88,63 @@ def login():
     # Validation
     if request.method == 'POST' and form.validate():
 
+        # Forget any user_id
+        session.clear()
+
         # Grab user data
         user = form.username.data
         psw = form.password.data
 
-        # debug
-        print(user, psw)
-    # TODO: check if data are in database,
-    # TODO: dehash password for chacking without storing the value in a var,
-    # TODO: redirect to dashbord if login is successful and message the user,
-    # TODO: dipslay a message if error occure
-    # TODO: Session
+        access = Users.query.filter_by(username=user).first()
+
+        # Check username
+        if access == None:
+            flash('Username not found, please register', 'danger')
+            return redirect('/register')
+
+        if access.password == None or not check_password_hash(access.password, psw):
+            flash('Incorrect password', 'danger')
+            return redirect('/login')
+
+        # Remeber which user logged in
+        session['user_id'] = access.id
+        session['user'] = access.username
+        session['logged_in'] = True
+
+        flash('You successfully logged in', 'success')
+        return redirect('/dashboard')
+
     return render_template('login.html', form=form)
+
+
+# Protect pages where log in is required
+def login_required(f):
+    ''' Decorate route where log in is required '''
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user_id') is None:
+            flash('Invalid access, log in', 'danger')
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 # Route to logout
 @app.route('/logout')
+@login_required
 def logout():
-    # TODO: logout from session
+    ''' User Log Out '''
+
+    # Forget the access
+    session.clear()
+
     return redirect('/')
 
 
 # Route to dashboard
 @app.route('/dashboard')
+@login_required
 def dashboard():
     # TODO: only if logged in
     return render_template('dashboard.html')
